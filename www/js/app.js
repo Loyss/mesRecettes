d7b1333669255eff9901cef4f28be6349d556fff
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2016-06-14 20:46:50 +0200

    single recette presque fonctionnel

--- www/js/app.js
+++ www/js/app.js
@@ -61,7 +61,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 }
             })
             .state('app.recette', {
-                url: '/recette',
+                url: '/recettes/:recette_id',
                 views: {
                     'menuContent' : {
                         templateUrl: 'templates/recette.html',
